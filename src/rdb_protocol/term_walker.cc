totalwindupflightsystems/rethinkdb

commit 247806db11cf51b8337c5a7fc532cceb7823d3f3

changing backtraces to use ids rather than inefficient arrays

src/rdb_protocol/term_walker.cc
@@ -1,6 +1,7 @@
 // Copyright 2010-2014 RethinkDB, all rights reserved.
 #include "rdb_protocol/term_walker.hpp"
 
+#include "rdb_protocol/backtrace.hpp"
 #include "rdb_protocol/error.hpp"
 #include "rdb_protocol/minidriver.hpp"
 #include "rdb_protocol/pseudo_time.hpp"
@@ -12,31 +13,64 @@ namespace ql {
 // We use this class to walk a term and do something to every node.
 class term_walker_t {
 public:
-    // This constructor fills in the backtraces of a term (`walk`) and checks
-    // that it's well-formed with regard to write placement.
-    explicit term_walker_t(Term *root)
-        : depth(0), writes_legal(true), bt(0) {
-        walk(root, 0, head_frame);
+    // This constructor checks that the term-tree is well-formed.
+    term_walker_t(Term *root, backtrace_registry_t *_bt_reg) :
+            bt_reg(_bt_reg) {
+        frame_t toplevel_frame(&frames, root->type(), true, backtrace_id_t::empty());
+        walk(root, &toplevel_frame);
     }
 
-    // This constructor propagates a backtrace down a tree until it hits a node
-    // that already has a backtrace (this is used for e.g. rewrite terms so that
-    // they return reasonable backtraces in the macroexpanded nodes).
-    term_walker_t(Term *root, const Backtrace *_bt)
-        : depth(0), writes_legal(true), bt(_bt) {
-        propwalk(root, 0, head_frame);
+    // Propagates a backtrace down a tree until it hits a node that already has a
+    // backtrace (this is used for e.g. rewrite terms so that they return reasonable
+    // backtraces in the macroexpanded nodes).
+    term_walker_t(Term *root, backtrace_id_t bt) :
+            bt_reg(nullptr) {
+        frame_t toplevel_frame(&frames, root->type(), true, bt);
+        walk(root, &toplevel_frame);
     }
+
     ~term_walker_t() {
-        r_sanity_check(depth == 0);
-        r_sanity_check(writes_legal == true);
+        r_sanity_check(frames.empty());
     }
 
-    void walk(Term *t, Term *parent, backtrace_t::frame_t frame) {
-        r_sanity_check(!bt);
+    // Build up an intrusive list stack for reporting backtraces
+    // without compiling the terms or using much dynamic memory.
+    class frame_t : public intrusive_list_node_t<frame_t> {
+    public:
+        frame_t(intrusive_list_t<frame_t> *_parent_list,
+                Term::TermType _term_type,
+                bool is_zeroth_argument,
+                backtrace_id_t _bt) :
+            parent_list(_parent_list), term_type(_term_type),
+            bt(_bt), writes_legal(true)
+        {
+            frame_t *prev_frame = parent_list->tail();
+            if (prev_frame != nullptr) {
+                writes_legal = prev_frame->writes_legal &&
+                    (is_zeroth_argument || !term_forbids_writes(prev_frame->term_type));
+            }
+            parent_list->push_back(this);
+        }
 
-        val_pusher_t<int> depth_pusher(&depth, depth+1);
-        add_bt(t, parent, frame);
+        ~frame_t() {
+            parent_list->remove(this);
+        }
 
+        intrusive_list_t<frame_t> *parent_list;
+        const Term::TermType term_type;
+        backtrace_id_t bt;
+
+        // True if writes are still legal at this node.  Basically:
+        // * Once writes become illegal, they are never legal again.
+        // * Writes are legal at the root.
+        // * If the parent term forbids writes in its function arguments AND we
+        //   aren't inside the 0th argument, writes are forbidden.
+        // * Writes are legal in all other cases.
+        bool writes_legal;
+    };
+
+    void walk(Term *t, frame_t *this_frame) {
+        guarantee(this_frame != nullptr);
         if (t->type() == Term::NOW && t->args_size() == 0) {
             // Construct curtime the first time we access it
             if (!curtime.has()) {
@@ -46,60 +80,54 @@ public:
         }
 
         if (t->type() == Term::ASC || t->type() == Term::DESC) {
-            rcheck_src(&t->GetExtension(ql2::extension::backtrace),
-                       parent && parent->type() == Term::ORDER_BY,
-                       base_exc_t::GENERIC,
-                       strprintf("%s may only be used as an argument to ORDER_BY.",
-                                 (t->type() == Term::ASC ? "ASC" : "DESC")));
+            const frame_t *prev_frame = frames.prev(this_frame);
+            if (prev_frame != nullptr && prev_frame->term_type != Term::ORDER_BY) {
+                throw bt_exc_t(Response::COMPILE_ERROR,
+                    strprintf("%s may only be used as an argument to ORDER_BY.",
+                              (t->type() == Term::ASC ? "ASC" : "DESC")),
+                    bt_reg->datum_backtrace(this_frame->bt));
+            }
         }
 
-        bool writes_still_legal = writes_are_still_legal(parent, frame);
-        rcheck_src(&t->GetExtension(ql2::extension::backtrace),
-                   writes_still_legal || !term_is_write_or_meta(t),
-                   base_exc_t::GENERIC,
-                   strprintf("Cannot nest writes or meta ops in stream operations.  "
-                             "Use FOR_EACH instead."));
-        val_pusher_t<bool> writes_legal_pusher(&writes_legal, writes_still_legal);
+        if (term_is_write_or_meta(t->type()) && !this_frame->writes_legal) {
+            throw bt_exc_t(Response::COMPILE_ERROR,
+                strprintf("Cannot nest writes or meta ops in stream operations.  Use "
+                          "FOR_EACH instead."),
+                bt_reg->datum_backtrace(this_frame->bt));
+        }
 
-        term_recurse(t, &term_walker_t::walk);
-    }
+        if (t->HasExtension(ql2::extension::backtrace_id)) {
+            return; // This node has been visited already
+        }
 
-    void propwalk(Term *t, UNUSED Term *parent, UNUSED backtrace_t::frame_t frame) {
-        r_sanity_check(bt);
+        t->SetExtension(ql2::extension::backtrace_id, this_frame->bt.get());
 
-        if (!t->HasExtension(ql2::extension::backtrace)) {
-            *t->MutableExtension(ql2::extension::backtrace) = *bt;
-            term_recurse(t, &term_walker_t::propwalk);
+        for (int i = 0; i < t->args_size(); ++i) {
+            Term *child = t->mutable_args(i);
+            backtrace_id_t bt = child_bt(this_frame, datum_t(static_cast<double>(i)));
+            frame_t child_frame(&frames, child->type(), i == 0, bt);
+            walk(child, &child_frame);
+        }
+
+        for (int i = 0; i < t->optargs_size(); ++i) {
+            Term_AssocPair *ap = t->mutable_optargs(i);
+            Term *child = ap->mutable_val();
+            backtrace_id_t bt = child_bt(this_frame, datum_t(ap->key().c_str()));
+            frame_t child_frame(&frames, child->type(), false, bt);
+            walk(child, &child_frame);
         }
     }
 private:
-    // Recurses to child terms.
-    void term_recurse(Term *t, void (term_walker_t::*callback)(Term *, Term *,
-                                                                backtrace_t::frame_t)) {
-        for (int i = 0; i < t->args_size(); ++i) {
-            (this->*callback)(t->mutable_args(i), t, backtrace_t::frame_t(i));
+    backtrace_id_t child_bt(const frame_t *this_frame, datum_t val) {
+        if (bt_reg == nullptr) {
+            return this_frame->bt;
         }
-        for (int i = 0; i < t->optargs_size(); ++i) {
-            Term_AssocPair *ap = t->mutable_optargs(i);
-            (this->*callback)(ap->mutable_val(), t, backtrace_t::frame_t(ap->key()));
-        }
-    }
-
-    // Adds a backtrace to a term.
-    void add_bt(Term *t, Term *parent, backtrace_t::frame_t frame) {
-        r_sanity_check(t->ExtensionSize(ql2::extension::backtrace) == 0);
-        if (parent) {
-            *t->MutableExtension(ql2::extension::backtrace)
-                = parent->GetExtension(ql2::extension::backtrace);
-        } else {
-            r_sanity_check(frame.is_head());
-        }
-        *t->MutableExtension(ql2::extension::backtrace)->add_frames() = frame.toproto();
+        return bt_reg->new_frame(this_frame->bt, val);
     }
 
     // Returns true if `t` is a write or a meta op.
-    static bool term_is_write_or_meta(Term *t) {
-        switch (t->type()) {
+    static bool term_is_write_or_meta(Term::TermType type) {
+        switch (type) {
         case Term::UPDATE:
         case Term::DELETE:
         case Term::INSERT:
@@ -282,22 +310,8 @@ private:
         }
     }
 
-    // Returns true if writes are still legal at this node.  Basically:
-    // * Once writes become illegal, they are never legal again.
-    // * Writes are legal at the root.
-    // * If the parent term forbids writes in its function arguments AND we
-    //   aren't inside the 0th argument, writes are forbidden.
-    // * Writes are legal in all other cases.
-    bool writes_are_still_legal(Term *parent, backtrace_t::frame_t frame) {
-        if (!writes_legal) return false; // writes never become legal again
-        if (!parent) return true; // writes legal at root of tree
-        if (term_forbids_writes(parent) && frame.is_stream_funcall_frame()) {
-            return false;
-        }
-        return true;
-    }
-    static bool term_forbids_writes(Term *term) {
-        switch (term->type()) {
+    static bool term_forbids_writes(Term::TermType type) {
+        switch (type) {
         case Term::REDUCE:
         case Term::MAP:
         case Term::FILTER:
@@ -480,36 +494,17 @@ private:
         }
     }
 
-    // We use this class to change a value while recursing, then restore it to
-    // its previous value.
-    template<class T>
-    class val_pusher_t {
-    public:
-        val_pusher_t(T *_val_ptr, T new_val) : val_ptr(_val_ptr) {
-            old_val = *val_ptr;
-            *val_ptr = new_val;
-        }
-        ~val_pusher_t() {
-            *val_ptr = old_val;
-        }
-    private:
-        T old_val;
-        T *val_ptr;
-    };
-
-    int depth;
-    bool writes_legal;
-    const Backtrace *const bt;
+    backtrace_registry_t *bt_reg;
     datum_t curtime;
+    intrusive_list_t<frame_t> frames;
 };
 
-void preprocess_term(Term *root) {
-    term_walker_t walker(root);
+void preprocess_term(Term *root, backtrace_registry_t *bt_reg) {
+    term_walker_t walker(root, bt_reg);
 }
 
-void propagate_backtrace(Term *root, const Backtrace *bt) {
+void propagate_backtrace(Term *root, backtrace_id_t bt) {
     term_walker_t walker(root, bt);
 }
-
 
 }  // namespace ql

commit b286c7da0e94701cbbdacf855007104eed462108

Fix #4951
The bug got introduced in 247806db and persisted through the rewrite in
57c97f80

src/rdb_protocol/term_walker.cc
@@ -114,7 +114,7 @@ private:
 
             if (type == Term::ASC || type == Term::DESC) {
                 rcheck_src(bt,
-                    prev_frame == nullptr || prev_frame->type == Term::ORDER_BY,
+                    prev_frame != nullptr && prev_frame->type == Term::ORDER_BY,
                     base_exc_t::LOGIC,
                     strprintf("%s may only be used as an argument to ORDER_BY.",
                               (type == Term::ASC ? "ASC" : "DESC")));